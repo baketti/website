a58e42734777d39fe523ca1ff73e5b337dc93abf
Emanuele Bachetti <e.bachetti1997@gmail.com>
2023-10-15 15:58:41 +0200

    commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');//
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const indexRouter = require('./routes/index');
+
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://dbApp:dbapp@cluster0.9rogpvu.mongodb.net/");
+const db = mongoose.connection;
+db.on('error', error=>console.error(error));
+db.once('open',()=>console.log('Connected to Mongoose!'))
+
+app.set('view engine','ejs')
+app.set('views', __dirname+'\\views')
+app.set('layout','layouts\\layout')
+app.use(expressLayouts)
+app.use(express.static('public'));
+app.use('/', indexRouter);
+
+app.listen(process.env.PORT || 3000);
